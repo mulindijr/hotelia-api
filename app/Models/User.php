808260ce03f026e626720c9eb7efc51f1dc97508
Brian <mulindijrn@gmail.com>
2026-06-22 15:52:40 +0300

    feat(auth): add loginHistories relationship to User model
    - Defines hasMany relationship linking a User to their LoginHistory logs
    
    - Enables reverse lookup for auditing historical user session details

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return "{$this->first_name} {$this->last_name}";
     }
+
+    public function loginHistories()
+    {
+        return $this->hasMany(LoginHistory::class);
+    }
 }
