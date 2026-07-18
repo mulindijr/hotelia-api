3e41d799eac4c3c0df3e1ac535423c03dfa1655a
Brian <mulindijrn@gmail.com>
2026-07-18 23:06:13 +0300

    refactor(router): use Permissions constants in routes/api/housekeeping.php

--- routes/api/housekeeping.php
+++ routes/api/housekeeping.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Hotels\HousekeepingController;
+use App\Constants\Permissions;
 
 Route::middleware(['auth:sanctum'])->prefix('hotels/{hotel}/housekeeping')->group(function () {
-    Route::get('/', [HousekeepingController::class, 'index'])->middleware('permission:view housekeeping')->name('housekeeping.index');
-    Route::post('/', [HousekeepingController::class, 'store'])->middleware('permission:manage housekeeping')->name('housekeeping.store');
-    Route::get('/{task}', [HousekeepingController::class, 'show'])->middleware('permission:view housekeeping')->name('housekeeping.show');
-    Route::put('/{task}', [HousekeepingController::class, 'update'])->middleware('permission:manage housekeeping')->name('housekeeping.update');
-    Route::delete('/{task}', [HousekeepingController::class, 'destroy'])->middleware('permission:manage housekeeping')->name('housekeeping.destroy');
+    Route::get('/', [HousekeepingController::class, 'index'])->middleware('permission:' . Permissions::VIEW_HOUSEKEEPING)->name('housekeeping.index');
+    Route::post('/', [HousekeepingController::class, 'store'])->middleware('permission:' . Permissions::MANAGE_HOUSEKEEPING)->name('housekeeping.store');
+    Route::get('/{task}', [HousekeepingController::class, 'show'])->middleware('permission:' . Permissions::VIEW_HOUSEKEEPING)->name('housekeeping.show');
+    Route::put('/{task}', [HousekeepingController::class, 'update'])->middleware('permission:' . Permissions::MANAGE_HOUSEKEEPING)->name('housekeeping.update');
+    Route::delete('/{task}', [HousekeepingController::class, 'destroy'])->middleware('permission:' . Permissions::MANAGE_HOUSEKEEPING)->name('housekeeping.destroy');
 });
