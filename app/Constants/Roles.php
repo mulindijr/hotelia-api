4e5cfd3c5f5a2feb99bd106a8082f7f202ec4e82
Brian <mulindijrn@gmail.com>
2026-07-18 17:24:50 +0300

    feat(auth): define backing string Roles enum for RBAC

---
+++ app/Constants/Roles.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants;
+
+enum Roles: string
+{
+  case SUPER_ADMIN = 'super_admin';
+  case HOTEL_MANAGER = 'hotel_manager';
+  case RECEPTIONIST = 'receptionist';
+  case HOUSEKEEPER = 'housekeeper';
+  case ACCOUNTANT = 'accountant';
+
+  // Get all string values.
+  public static function all(): array
+  {
+    return array_column(self::cases(), 'value');
+  }
+}
