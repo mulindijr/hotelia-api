76e3a8310bdd0db9d110d25e2380beefc030019e
Brian <mulindijrn@gmail.com>
2026-07-18 23:05:22 +0300

    refactor(router): use Permissions constants in routes/api/services.php

--- routes/api/services.php
+++ routes/api/services.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Hotels\ServiceController;
+use App\Constants\Permissions;
 
 Route::middleware(['auth:sanctum'])->prefix('hotels/{hotel}/services')->group(function () {
-    Route::get('/', [ServiceController::class, 'index'])->middleware('permission:view services')->name('services.index');
-    Route::post('/', [ServiceController::class, 'store'])->middleware('permission:create services')->name('services.store');
-    Route::get('/{service}', [ServiceController::class, 'show'])->middleware('permission:view services')->name('services.show');
-    Route::put('/{service}', [ServiceController::class, 'update'])->middleware('permission:update services')->name('services.update');
-    Route::delete('/{service}', [ServiceController::class, 'destroy'])->middleware('permission:delete services')->name('services.destroy');
+    Route::get('/', [ServiceController::class, 'index'])->middleware('permission:' . Permissions::VIEW_SERVICES)->name('services.index');
+    Route::post('/', [ServiceController::class, 'store'])->middleware('permission:' . Permissions::CREATE_SERVICES)->name('services.store');
+    Route::get('/{service}', [ServiceController::class, 'show'])->middleware('permission:' . Permissions::VIEW_SERVICES)->name('services.show');
+    Route::put('/{service}', [ServiceController::class, 'update'])->middleware('permission:' . Permissions::UPDATE_SERVICES)->name('services.update');
+    Route::delete('/{service}', [ServiceController::class, 'destroy'])->middleware('permission:' . Permissions::DELETE_SERVICES)->name('services.destroy');
 });
