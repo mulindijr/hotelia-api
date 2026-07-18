be8f96d3eee49d7c1c51648b0720d50935ba5434
Brian <mulindijrn@gmail.com>
2026-07-18 23:04:32 +0300

    refactor(router): use Permissions constants in routes/api/bookings.php

--- routes/api/bookings.php
+++ routes/api/bookings.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Hotels\BookingController;
+use App\Constants\Permissions;
 
 Route::middleware(['auth:sanctum'])->prefix('hotels/{hotel}/bookings')->group(function () {
-    Route::get('/', [BookingController::class, 'index'])->middleware('permission:view bookings')->name('bookings.index');
-    Route::post('/', [BookingController::class, 'store'])->middleware('permission:create bookings')->name('bookings.store');
-    Route::get('/{booking}', [BookingController::class, 'show'])->middleware('permission:view bookings')->name('bookings.show');
-    Route::put('/{booking}', [BookingController::class, 'update'])->middleware('permission:update bookings')->name('bookings.update');
-    Route::post('/{booking}/cancel', [BookingController::class, 'cancel'])->middleware('permission:cancel bookings')->name('bookings.cancel');
-    Route::post('/{booking}/check-in', [BookingController::class, 'checkIn'])->middleware('permission:check in guests')->name('bookings.check-in');
-    Route::post('/{booking}/check-out', [BookingController::class, 'checkOut'])->middleware('permission:check out guests')->name('bookings.check-out');
+    Route::get('/', [BookingController::class, 'index'])->middleware('permission:' . Permissions::VIEW_BOOKINGS)->name('bookings.index');
+    Route::post('/', [BookingController::class, 'store'])->middleware('permission:' . Permissions::CREATE_BOOKINGS)->name('bookings.store');
+    Route::get('/{booking}', [BookingController::class, 'show'])->middleware('permission:' . Permissions::VIEW_BOOKINGS)->name('bookings.show');
+    Route::put('/{booking}', [BookingController::class, 'update'])->middleware('permission:' . Permissions::UPDATE_BOOKINGS)->name('bookings.update');
+    Route::post('/{booking}/cancel', [BookingController::class, 'cancel'])->middleware('permission:' . Permissions::CANCEL_BOOKINGS)->name('bookings.cancel');
+    Route::post('/{booking}/check-in', [BookingController::class, 'checkIn'])->middleware('permission:' . Permissions::CHECK_IN_GUESTS)->name('bookings.check-in');
+    Route::post('/{booking}/check-out', [BookingController::class, 'checkOut'])->middleware('permission:' . Permissions::CHECK_OUT_GUESTS)->name('bookings.check-out');
 });
