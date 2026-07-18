c3d3761426a08c23d47ec44adf4cca69bcef48a0
Brian <mulindijrn@gmail.com>
2026-07-18 23:07:01 +0300

    refactor(router): use Permissions constants in routes/api/maintenance.php

--- routes/api/maintenance.php
+++ routes/api/maintenance.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Hotels\MaintenanceController;
+use App\Constants\Permissions;
 
 Route::middleware(['auth:sanctum'])->prefix('hotels/{hotel}/maintenance')->group(function () {
-    Route::get('/', [MaintenanceController::class, 'index'])->middleware('permission:view maintenance')->name('maintenance.index');
-    Route::post('/', [MaintenanceController::class, 'store'])->middleware('permission:manage maintenance')->name('maintenance.store');
-    Route::get('/{maintenanceRequest}', [MaintenanceController::class, 'show'])->middleware('permission:view maintenance')->name('maintenance.show');
-    Route::put('/{maintenanceRequest}', [MaintenanceController::class, 'update'])->middleware('permission:manage maintenance')->name('maintenance.update');
-    Route::delete('/{maintenanceRequest}', [MaintenanceController::class, 'destroy'])->middleware('permission:manage maintenance')->name('maintenance.destroy');
+    Route::get('/', [MaintenanceController::class, 'index'])->middleware('permission:' . Permissions::VIEW_MAINTENANCE)->name('maintenance.index');
+    Route::post('/', [MaintenanceController::class, 'store'])->middleware('permission:' . Permissions::MANAGE_MAINTENANCE)->name('maintenance.store');
+    Route::get('/{maintenanceRequest}', [MaintenanceController::class, 'show'])->middleware('permission:' . Permissions::VIEW_MAINTENANCE)->name('maintenance.show');
+    Route::put('/{maintenanceRequest}', [MaintenanceController::class, 'update'])->middleware('permission:' . Permissions::MANAGE_MAINTENANCE)->name('maintenance.update');
+    Route::delete('/{maintenanceRequest}', [MaintenanceController::class, 'destroy'])->middleware('permission:' . Permissions::MANAGE_MAINTENANCE)->name('maintenance.destroy');
 });
