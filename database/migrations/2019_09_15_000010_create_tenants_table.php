f7f91ea3920b4c8a0c6c508be3004d2320e1e5c0
mulindijr <mulindijr@example.com>
2026-09-26 15:32:50 +0300

    chore: add tenants table migration with custom company columns

---
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->string('id')->primary();
+
+            // Custom columns for the tenant (Company)
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('subscription_plan')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->string('logo_url')->nullable();
+
+            $table->timestamps();
+            $table->json('data')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
